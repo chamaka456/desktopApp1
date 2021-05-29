d93ddca76b771c15b83a938581c837acbb98ff24
Dinith Jayabodhi <jdinith72@gmail.com>
2021-05-29 21:34:42 +0530

    added the controller and model structure to the project

---
+++ Manage Building-1/Manage Building/ConnectorClass.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manage_Building
+{
+    class ConnectorClass
+    {
+
+        private readonly SqlConnection con;
+
+        public ConnectorClass()
+        {
+            string connString = "Server=52.172.250.71;Database=AriyaDb;User Id=sa;Password=dj@innova;";
+            this.con = new SqlConnection(connString);
+        }
+
+        public void OpenConection()
+        {
+            
+            if (con != null && con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            else
+            {
+                Console.WriteLine("db connected");
+            }
+        }
+
+
+        private void CloseConnection()
+        {
+            if (con != null && con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+
+        public bool executequery(String Query)
+        {
+            try
+            {
+                OpenConection();
+                SqlCommand cmd = new SqlCommand(Query, con);
+                if (cmd.ExecuteNonQuery() == 1)
+                {
+                    return true;
+
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            finally
+            {
+                CloseConnection();
+            }
+
+        }
+
+        public object ShowDataInGridView(string Query_)
+        {
+            SqlDataAdapter dr = new SqlDataAdapter(Query_, con);
+            DataSet ds = new DataSet();
+            dr.Fill(ds);
+            object dataum = ds.Tables[0];
+            return dataum;
+        }
+
+
+        public SqlDataReader DataReader(string Query_, String para1)
+        {
+            SqlCommand cmd = new SqlCommand(Query_, con);
+            cmd.Parameters.AddWithValue("@building_name", para1);
+            SqlDataReader dr = cmd.ExecuteReader();
+            return dr;
+        }
+
+        public SqlDataReader DataReader(string Query)
+        {
+            SqlCommand cmd = new SqlCommand(Query, con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            return dr;
+        }
+
+
+        public int DataCount(string Tablename_ )
+        {
+            SqlCommand cmd = new SqlCommand("SELECT COUNT(*) FROM "+Tablename_, con);
+       
+            return (Int32)cmd.ExecuteScalar();
+        }
+
+    }
+}
---
+++ Manage Building-1/Manage Building/controller/BuildingController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manage_Building.controller
+{
+    public class BuildingController
+    {
+        private readonly ConnectorClass dbConnection;
+
+        public BuildingController()
+        {
+            dbConnection = new ConnectorClass();
+        }
+
+        public bool AddNewBuilding()
+        {
+            return false;
+        }
+    }
+}
