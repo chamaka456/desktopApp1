f01efafd8536b7cf899676d4213382ebdc0c23f9
Dinith Jayabodhi <jdinith72@gmail.com>
2021-05-30 10:51:27 +0530

    added the controller

---
+++ Manage Building-1/Manage Building/controller/SessionController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manage_Building.controller
+{
+    class SessionController
+    {
+        private readonly ConnectorClass dbConnection;
+
+        public SessionController()
+        {
+            this.dbConnection = new ConnectorClass();
+        }
+    }
+}
